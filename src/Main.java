bdc06ee85350fcdf1a05dc923a0ab0d6b5fd63c2
Josrodjr <Josrodjr@gmail.com>
2017-02-02 13:24:22 -0600

    Detonacion y Creacion
    Detonacion del lector por ubicacion del archivo y creacion de el metodo
    operar que funciona(SI)

--- src/Main.java
+++ src/Main.java
@@ -5,72 +5,22 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	
-	 private static Scanner s;
 	 
-	private static final String FILENAME = "C:/Users/Cristian/Desktop/Prueba.txt";
-	private static String sCurrentLine;
+	//private static final String FILENAME = "C:/Users/Cristian/Desktop/Prueba.txt";
+	//private static final String FILENAME = "C:/Users/Jose/Documents/GitHub/Hoja-2/src/Prueba.txt";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-
-	    s = new Scanner(System.in);
 	    System.out.println("Bienvenido al programa de calculadora!");
-	    
-
-		BufferedReader br = null;
-		FileReader fr = null;
-
-	
-
-			try {
-				fr = new FileReader(FILENAME);
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			br = new BufferedReader(fr);
-
-			
-
-			try {
-				br = new BufferedReader(new FileReader(FILENAME));
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			try {
-				sCurrentLine = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			
-			//primer numero
-		System.out.println(sCurrentLine);
-		String a =sCurrentLine.trim();
-		System.out.println(a);
-		char[] b = a.toCharArray();
-		char c = b[0];
-		System.out.println(c);
-		int juan = Integer.parseInt((String.valueOf(c)));
-		System.out.println(juan);
 		
-		char d = b[2];
-		System.out.println(d);
-		int dos = Integer.parseInt((String.valueOf(d)));
+		BufferedReader file = new BufferedReader(new FileReader("Prueba.txt"));
+		Op calculadora = new Op();
+		String operacion = file.readLine();
+		calculadora.operar(operacion);
+		System.out.println(operacion);
 		
-		char e = b[4];
-				System.out.println(e);
-				String operador = String.valueOf(e);
-				if(operador == "+"){
-					System.out.println(operador);
-					int solucion = juan+dos;
-					System.out.println(solucion);
-				}
-			
-				
-				
+		//me detone todo eso wei porque ahora pones afuera del git el archivo y funciona
+		
 			
 				
 	}
--- src/Op.java
+++ src/Op.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Op implements Calculadora {
 
 
@@ -31,6 +33,46 @@ public class Op implements Calculadora {
 		return a/b;
 	}
 
+	public double operar(String n){
+		
+		/**
+		String Operator = "";
+		int numero1 = 0;
+		int numero2 =0;
+		double result = 0;
+		char c;
+		numero1 = 0;
+		numero2 = 0;
+		*/
+		
+		StackArrayList datos = new StackArrayList();
+		Scanner lector = new Scanner(n);
+		
+		while(lector.hasNext()){
+			if(lector.hasNextInt()){
+				datos.push(lector.nextInt());
+			}
+			else{
+				int numero1, numero2;
+				numero1 = (int) datos.pop();
+				numero2 = (int) datos.pop();
+				String operando = lector.next();
+				
+				switch (operando){
+				case "+": 
+					datos.push(suma(numero1, numero2));
+					break;
+				case "-": 
+					datos.push(resta(numero1, numero2));
+				}
+				
+			}
+		}
 	
+		double respuesta = (double) datos.pop();
+		return respuesta;
+	
+		
+	}
 
 }
